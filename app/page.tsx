a84b5879c10ee759b715d456efe32faef6987e1c
ilkinhuseynliilkin296-cloud <ilkinhuseynliilkin296@gmail.com>
2026-08-24 18:58:29 +0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <html>
+      <body style={{ textAlign: 'center', padding: '50px' }}>
+        <h1>🎉 网站部署成功啦！</h1>
+        <p>恭喜你！网站可以正常访问了！</p>
+      </body>
+    </html>
+  )
+}
